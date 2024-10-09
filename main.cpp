75ba6cdfb0fd22a60dbc010abf4f4f01d5ae6ec8
Simone-Gramuglia <simone.gramuglia@marconibari.edu.it>
2024-10-09 05:52:09 +0000

    consegnato

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+float calcolaArea (float r){
+   float area;
+   area = 3.14 * (r*r);
+   return area;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+float calcolaCirconferenza (float r){
+   float circo;
+   circo = 3.14 * (r*2);
+   return circo;
+}
+
+int main() {
+    float r1, r2, area1, area2, circo1, circo2;
+    cout << "Inserisci il raggio del cerchio 1";
+    cin >> r1;
+    cout << "Inserisci il raggio del cerchio 2";
+    cin >> r2;
+    area1 = calcolaArea(r1);
+    area2 = calcolaArea(r2);
+    circo1 = calcolaCirconferenza(r1);
+    circo2 =calcolaCirconferenza(r2);
+    cout << "L'area del cerchio 1 è: " << area1 << endl;
+    cout << "L'area del cerchio 2 è: " << area2 << endl;
+    cout << "La circonferenza del cerchio 1 è: " << circo1 << endl;
+    cout << "La circonferenza del cerchio 2 è: " << circo2 << endl;
+
+
+}
+
+
